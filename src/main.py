fd9b48ae902bb7f11414e665633ee9f7eb5b36e5
abeldonate <abeldm3108@gmail.com>
2025-06-27 11:02:56 +0200

    first commit

---
+++ src/main.py
@@ -0,0 +1,25 @@
+import os
+import datetime
+from flask import Flask, render_template, url_for
+
+template_path = os.path.abspath("src/templates")
+static_path = os.path.abspath("src/static")
+
+app = Flask(
+    __name__,
+    template_folder=template_path,
+    static_folder=static_path,
+)
+
+
+@app.route('/')
+def page_index():
+    return render_template('index.html')
+
+@app.route('/activities')
+def activities():
+    return render_template('activities.html')
+
+
+if __name__ == '__main__':
+    app.run(port=5000, host='0.0.0.0')
